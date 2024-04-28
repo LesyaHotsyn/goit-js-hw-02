c37e065bb5ebb9aaf8491393e0e14a26e7402626
Hotsyn <85835454+LesyaHotsyn@users.noreply.github.com>
2024-04-28 21:12:31 +0300

    + js task 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
-function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-   // console.log(totalPrice);
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
+// function makeTransaction(quantity, pricePerDroid) {
+//     let totalPrice = quantity * pricePerDroid;
+//    // console.log(totalPrice);
+//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
